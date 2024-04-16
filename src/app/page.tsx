87f1884de1f152147c4752d4e08db00368d74b58
fogracvxy <marn1002000@gmail.com>
2024-04-16 16:01:00 +0200

    connected images to users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,34 @@
 import Link from "next/link";
 import { db } from "~/server/db";
-
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
-  console.log(images);
-  return (
-    <main className="">
+  const Images = async () => {
+    const images = await db.query.images.findMany({
+      orderBy: (model, { desc }) => desc(model.id),
+    });
+    return (
       <div className="flex flex-wrap gap-4">
-        {images.map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
+        {images.map((image) => (
+          <div key={image.id} className="flex w-48 flex-col">
             <img src={image.url} />
-            <div>{image.name}</div>
+            {/* <div>{image.name}</div> */}
           </div>
         ))}
       </div>
+    );
+  };
+
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="text-center">Please sign in!</div>
+      </SignedOut>
+      <SignedIn>
+        {" "}
+        <Images />
+      </SignedIn>
     </main>
   );
 }
